51e0ad0f88b69c5f38d15c1bafd6925bfef59742
SMN <SMN@SMN-PC>
2022-06-15 20:39:48 +0300

    Task2

---
+++ Task2/Task2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using System.IO;
+
+namespace task2
+{
+
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Укажите каталог:");
+            string path = Console.ReadLine();
+            Console.WriteLine("Размер директории: {0} байт", SizeFolder(path));
+        }
+        public static long SizeFolder(string folderpath)
+        {
+
+            long size = 0;
+            if (Directory.Exists(folderpath))
+            {
+                try
+                {
+                    foreach (string file in Directory.GetFiles(folderpath))
+                    {
+                        if (File.Exists(file))
+                        {
+                            FileInfo fi = new FileInfo(file);
+                            size += fi.Length;
+                        }
+                    }
+                    foreach (string dirs in Directory.GetDirectories(folderpath))
+                    {
+                        size += SizeFolder(dirs);
+                    }
+                }
+                catch (Exception ex)
+                { Console.WriteLine($"Не удалось посчитать размер папки, ошибка {ex.Message}"); }
+            }
+            else
+            {
+                size = 0;
+                Console.WriteLine("Нет доступа!");
+            }
+            return size;
+        }
+    }
+}
